23e0f6ae48624f3b524be4ccf290a6d7494caac4
peternguy <pjflipstudio@gmail.com>
2025-01-06 23:28:36 -0600

    inital commit

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Hpliondancechicago
+Hpliondancechicago
---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Hero from './components/Hero.jsx'
+
+
+const App = () => {
+  return (
+    <main>
+      <main className='relative min-h-screen w-screen overflow-x-hidden'>
+        <Hero />
+      </main>
+
+    </main>
+  )
+}
+
+export default App
