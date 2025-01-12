510a7dcc0c7a9abd4399b9aecb70a7c9faeede53
peternguy <pjflipstudio@gmail.com>
2025-01-11 18:54:05 -0600

    fixing refresh

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <Router basename="/Hpliondancechicago/">
+    <Router basename="/Hpliondancechicago">
       <App />
     </Router>
   </React.StrictMode>
